ff7e70981bc1b667885088bf609d5372d623d3a8
spieta <spieta2@gmail.com>
2021-01-13 11:19:57 +0100

    Page init

--- app/src/main/java/com/pieta/jumpscounter/logic/JumpDetector.java
+++
@@ -1,8 +0,0 @@
-package com.pieta.jumpscounter.logic;
-
-public interface JumpDetector {
-    void setCollector(JumpCollector collector);
-    void updateCollector();
-    void startDetecting();
-    void stopDetecting();
-}
